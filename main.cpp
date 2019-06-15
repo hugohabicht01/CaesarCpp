3bc6d9efd2d8a177cce0ecdf727109db2683d7a0
linuxer <hugohabicht01@gmx.net>
2019-06-15 18:06:40 +0200

    Added argh

--- main.cpp
+++ main.cpp
@@ -43,74 +43,26 @@ string replace(string input, map<char, char>& alphabetMap) {
 int main(int argc, const char** argv) {
     map<char ,char> alphabet;
     int rotations {};
+    string rotationsString {};
     string filename {};
     string input {};
-
-
-    args::ArgumentParser parser("This is a program to de-/encrypt caesar encryption");
-    args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
-    args::ValueFlag<int> rotationsArg(parser, "rotations", "The rotations flag", {'r'});
-    args::ValueFlag<string> fileArg(parser, "file", "Input file instead of using stdin", {'f'});
-    args::PositionalList<string> inputArg(parser, "input text", "The input text to de-/encrypt");
-    try
-    {
-        parser.ParseCLI(argc, argv);
-    }
-    catch (const args::Completion& e)
-    {
-        std::cout << e.what();
-        return 0;
-    }
-    catch (const args::Help&)
-    {
-        std::cout << parser;
-        return 0;
-    }
-    catch (const args::ParseError& e)
-    {
-        std::cerr << e.what() << std::endl;
-        std::cerr << parser;
-        return 1;
-    }
-
-    if(!rotationsArg) {
-        cerr << "The rotations argument is missing";
-        cerr << parser;
-        return 1;
-    }
-
-    if (!inputArg && !fileArg) {
-        cerr << "The text or file argument is missing";
-        cerr << parser;
-        return 1;
-    }
-
-    if (fileArg && inputArg) {
-        cerr << "An error occured, You can not use a file as input and a string at the same time";
-        cerr << parser;
-        return 1;
-    }
-    rotations = args::get(rotationsArg);
-    if (inputArg) {
-        for (auto c: inputArg) {
-            input += c;
+    argh::parser cmdl;
+    cmdl.add_params({"-r", "--rotations", "-f", "--file", "-t", "--text"});
+    cmdl.parse(argc, argv);
+    if (cmdl("rotations") || cmdl("r")) {
+        try {
+            rotations = stoi(cmdl("rotations").str());
+            cout << rotations;
         }
-    } else if (fileArg) {
-        filename = args::get(fileArg);
-        ifstream inputfile (filename);
-        string line;
-        if (inputfile.is_open())
-        {
-            while ( getline (inputfile,line) )
-            {
-                input += line;
-            }
-            inputfile.close();
+        catch (int e) {
+            rotations = stoi(cmdl("r").str());
         }
     }
+    /*
     transform(input.begin(), input.end(), input.begin(), ::tolower);
 
     alphabet = rotate(rotations);
     cout << replace(input, alphabet);
+     */
     return 0;
 }
