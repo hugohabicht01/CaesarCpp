a9f5eae6f2c8be163a417540227d9d4134c34818
hugohabicht01 <hugohabicht01@gmx.net>
2019-06-16 15:38:47 +0200

    Shortened the code, but in one case "z" is displayed wrong

--- main.cpp
+++ main.cpp
@@ -7,98 +7,71 @@
 using namespace std;
 
 void readFile(const string& file, string *dest){
-    string line;
-    ifstream inputFile(file);
-    if (inputFile.is_open()) {
-        while (getline(inputFile, line)) {
-            *dest += line;
+        string line;
+        ifstream inputFile(file);
+        if (inputFile.is_open()) {
+                while (getline(inputFile, line)) {
+                        *dest += (line + "\n");
+                }
+                inputFile.close();
         }
-        inputFile.close();
-    }
 }
 
 map<char, char> rotate(int rotations){
-    map<char , char> alphabetMap {};
-    char originalChar, rotatedChar;
-    int valueOfChar;
-    const string alphabet_ascii_lowercase = "abcdefghijklmnopqrstuvwxyz";
-    for (int index = 0; index < 25; ++index) {
-        originalChar = alphabet_ascii_lowercase[index];
-        valueOfChar = originalChar - 97;
-        rotatedChar = (char) (((valueOfChar + rotations) % 26) + 97);
-        alphabetMap[originalChar] = rotatedChar;
-    }
-    return alphabetMap;
+        map<char , char> alphabetMap {};
+        char originalChar, rotatedChar;
+        int valueOfChar;
+        const string alphabet_ascii_lowercase = "abcdefghijklmnopqrstuvwxyz";
+        for (int index = 0; index < 25; ++index) {
+                originalChar = alphabet_ascii_lowercase[index];
+                valueOfChar = originalChar - 97;
+                rotatedChar = (char) (((valueOfChar + rotations) % 26) + 97);
+                alphabetMap[originalChar] = rotatedChar;
+        }
+        return alphabetMap;
 }
 
 
 string replace(string input, map<char, char>& alphabetMap) {
-    string replacedText {};
-    char replacedChar {};
+        string replacedText {};
+        char replacedChar {};
 
-    for (int index = 0; index < input.length(); ++index) {
-        char c = input[index];
-        if ('a' <= c && c <= 'z') {
-            replacedChar = alphabetMap.find(c)->second;
-        } else {
-            replacedChar = c;
+        for (int index = 0; index < input.length(); ++index) {
+                char c = input[index];
+                if ('a' <= c && c <= 'z') {
+                        replacedChar = alphabetMap.find(c)->second;
+                } else {
+                        replacedChar = c;
+                }
+                replacedText += replacedChar;
         }
-        replacedText += replacedChar;
-    }
-    return replacedText;
+        return replacedText;
 }
 
 
 
 int main(int argc, const char** argv) {
-    map<char ,char> alphabet;
-    int rotations {};
-    string rotationsString {};
-    string filename {};
-    string input {};
-    argh::parser cmdl;
-    cmdl.add_params({"-r", "--rotations", "-f", "--file", "-t", "--text"});
-    cmdl.parse(argc, argv);
-    if (cmdl("rotations") || cmdl("r")) {
-
-        if(cmdl("rotations")) {
-            rotations = stoi(cmdl("rotations").str());
-        } else if (cmdl("r")) {
-            rotations = stoi(cmdl("r").str());
+        map<char ,char> alphabet;
+        int rotations {};
+        string rotationsString {};
+        string filename {};
+        string input {};
+        argh::parser cmdl;
+        cmdl.add_params({"-r", "--rotations", "-f", "--file", "-t", "--text"});
+        cmdl.parse(argc, argv);
+        if (!(cmdl({"rotations", "r"}) >> rotations)) {
+                cerr << "Rotations argument missing.\n";
+                return 1;
+        }
+        if (cmdl({"text", "t"}) >> input) {
+        } else if (cmdl({"file", "f"}) >> filename) {
+                readFile(filename, &input);
         } else {
-            cerr << "Rotations argument missing.\n";
-            return 1;
+                cerr << "You have to specify a text or file argument. \n";
+                return 1;
         }
-
-    } else {
-        cerr << "Rotations argument missing.\n";
-        return 1;
-    }
-
-    if (cmdl("text") || cmdl("t")) {
-        if ("text") {
-            input = cmdl("text").str();
-        } else if (cmdl("t")) {
-            input = cmdl("t").str();
-        }
-
-    } else if (cmdl("file") || cmdl("f")) {
-        if (cmdl("file")) {
-            filename = cmdl("file").str();
-            readFile(filename, &input);
-        } else if (cmdl("f")) {
-            filename = cmdl("f").str();
-            readFile(filename, &input);
-        }
-
-    } else {
-        cerr << "You have to specify a text or file argument. \n";
-    }
-
-    transform(input.begin(), input.end(), input.begin(), ::tolower);
-
-    alphabet = rotate(rotations);
-    cout << replace(input, alphabet);
-
-    return 0;
+        transform(input.begin(), input.end(), input.begin(), ::tolower);
+        alphabet = rotate(rotations);
+        cout << replace(input, alphabet);
+        return 0;
 }
